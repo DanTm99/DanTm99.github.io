3c7b4930dc5919fc96d776e18811ca0855236809
DanTm99 <danyaal99@hotmail.co.uk>
2019-09-16 18:20:03 +0100

    Remove map marker and map javascript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -152,26 +152,6 @@ Table Of Contents end
 
         /* END SECTIONS BACKGROUNDS */
 
-        /* 10. START GOOGLE MAP */
-
-        function initialize() {
-            var mapOptions = {
-                zoom: 11,
-                scrollwheel: false,
-                center: new google.maps.LatLng(40.7143528, -74.0059731)
-            };
-            var map = new google.maps.Map(document.getElementById('map'),
-                mapOptions);
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(40.7143528, -74.0059731),
-                animation: google.maps.Animation.BOUNCE,
-                icon: 'assets/img/map-marker.png',
-                map: map
-            });
-        }
-        google.maps.event.addDomListener(window, 'load', initialize);
-
-        /* END GOOGLE MAP */
 
     });
 
